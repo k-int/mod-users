a849020ff81d7a3c1fbdb2bb92041a7e4974aaf5
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2016-11-18 09:52:16 -0500

    Minor changes to stub get endpoint

--- src/main/java/org/folio/rest/impl/UsersMultiAPI.java
+++ src/main/java/org/folio/rest/impl/UsersMultiAPI.java
@@ -93,8 +93,10 @@ public class UsersMultiAPI implements UsersMultiResource {
   }
 
   @Override
-  public void getUsersMulti(Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public void getUsersMulti(String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+    asyncResultHandler.handle(Future.succeededFuture(
+             PostUsersMultiResponse.withPlainInternalServerError(
+                     messages.getMessage(lang, MessageConsts.InternalServerError))));
   }
   
 }
